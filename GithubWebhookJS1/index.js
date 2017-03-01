ea5bd5d858ab8565da948eb253a2e1547f91e7b8
Louis Syu <louis_syu@wistron.com>
2017-03-01 09:35:17 +0800

    fix wording

--- GithubWebhookJS1/index.js
+++ GithubWebhookJS1/index.js
@@ -1,6 +1,6 @@
 // Please visit http://go.microsoft.com/fwlink/?LinkID=761099&clcid=0x409 for more information on settting up Github Webhooks
 module.exports = function (context, data) {
-    context.log('GiddtHub  triggered!', data.name+"  "+data.comment.body );
+    context.log('GiddtHub aa  triggered!', data.name+"  "+data.comment.body );
     context.res = { body: 'New GitHub comment: ' + data.comment.body };
     context.done();
 
